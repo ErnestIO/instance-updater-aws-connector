01c062d1fd4e23fab7625120733a40de7239f4c1
Tom Bevan <tom.bevan@r3labs.io>
2016-07-12 19:09:49 +0100

    working instance updating, no tests

---
+++ event.go
@@ -0,0 +1,52 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package main
+
+import (
+	"encoding/json"
+	"log"
+)
+
+// Event stores the network create data
+type Event struct {
+	ID                    string `json:"id"`
+	DatacenterVPCID       string `json:"datacenter_vpc_id"`
+	DatacenterRegion      string `json:"datacenter_region"`
+	DatacenterAccessKey   string `json:"datacenter_access_key"`
+	DatacenterAccessToken string `json:"datacenter_access_token"`
+	NetworkAWSID          string `json:"network_aws_id"`
+	SecurityGroupAWSID    string `json:"security_group_aws_id"`
+	InstanceAWSID         string `json:"instance_aws_id,omitempty"`
+	InstanceName          string `json:"instance_name"`
+	InstanceImage         string `json:"instance_image"`
+	InstanceType          string `json:"instance_type"`
+	ErrorMessage          string `json:"error,omitempty"`
+}
+
+// Valid checks if all criteria are met
+func (ev *Event) Valid() bool {
+	return true
+}
+
+// Error the request
+func (ev *Event) Error(err error) {
+	log.Printf("Error: %s", err.Error())
+	ev.ErrorMessage = err.Error()
+
+	data, err := json.Marshal(ev)
+	if err != nil {
+		log.Panic(err)
+	}
+	nc.Publish("instance.update.aws.error", data)
+}
+
+// Complete the request
+func (ev *Event) Complete() {
+	data, err := json.Marshal(ev)
+	if err != nil {
+		ev.Error(err)
+	}
+	nc.Publish("instance.update.aws.done", data)
+}
--- main.go
+++ main.go
@@ -5,15 +5,71 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"os"
 	"runtime"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/nats-io/nats"
 )
 
 var nc *nats.Conn
 var natsErr error
+
+func processEvent(data []byte) (*Event, error) {
+	var ev Event
+	err := json.Unmarshal(data, &ev)
+	return &ev, err
+}
+
+func eventHandler(m *nats.Msg) {
+	i, err := processEvent(m.Data)
+	if err != nil {
+		nc.Publish("instance.update.aws.error", m.Data)
+		return
+	}
+
+	if i.Valid() == false {
+		i.Error(errors.New("Instance is invalid"))
+		return
+	}
+
+	err = updateInstance(i)
+	if err != nil {
+		i.Error(err)
+		return
+	}
+
+	i.Complete()
+}
+
+func updateInstance(ev *Event) error {
+	creds := credentials.NewStaticCredentials(ev.DatacenterAccessKey, ev.DatacenterAccessToken, "")
+	svc := ec2.New(session.New(), &aws.Config{
+		Region:      aws.String(ev.DatacenterRegion),
+		Credentials: creds,
+	})
+
+	req := ec2.ModifyInstanceAttributeInput{
+		InstanceId: aws.String(ev.InstanceAWSID),
+		InstanceType: &ec2.AttributeValue{
+			Value: aws.String(ev.InstanceType),
+		},
+	}
+
+	_, err := svc.ModifyInstanceAttribute(&req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
 	natsURI := os.Getenv("NATS_URI")
@@ -26,11 +82,8 @@ func main() {
 		log.Fatal(natsErr)
 	}
 
-	nc.Subscribe("instance.update.aws", notImplemented)
+	fmt.Println("listening for instance.update.aws")
+	nc.Subscribe("instance.update.aws", eventHandler)
 
 	runtime.Goexit()
-}
-
-func notImplemented(m *nats.Msg) {
-	nc.Publish("instance.update.aws.error", []byte(`{"error":"not implemented"}`))
 }
